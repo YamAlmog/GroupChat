b2b5351a97daafcb0f6f7c5420fff1f720315167
YamAlmog <yamalmog14@gmail.com>
2023-12-09 20:21:54 +0200

    fix the send and receive function to be more efficient and readable, use  InvalidNameException exception for case the user input empty name

--- client.py
+++ client.py
@@ -2,72 +2,62 @@ import socket
 import logging
 import argparse
 import threading
+from error import InvalidNameException
+
 END_CHAT = 'exit'
 HEADER=1024
 
 logging.basicConfig(filename='app.log', filemode='w',  level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def send_message(socket):
-    try:    
-        while True:
-            msg_to_send = input('')
+    while True:
+        msg_to_send = input('')
+        try:    
             socket.send(msg_to_send.encode())
-            if msg_to_send.lower() == END_CHAT:
-                print("Closing the chat...")
-                break
-    except OSError:
-        raise OSError("Socket error occurred")
-    except Exception:
-        raise Exception("Unknown error occurred")
-    finally:
-        socket.close()
+        except socket.error as e:
+            raise OSError(f"Error with sending data:{e}")
+        if msg_to_send.lower() == END_CHAT:
+            print("Closing the chat...")
+            socket.close()
+            break
 
-def receive_message(socket, friend_name):
-    try:    
-        while True: 
-            
+
+def receive_message(socket):   
+    while True: 
+        try: 
             friend_msg = socket.recv(HEADER).decode('utf-8')
-            print(f"{friend_name}: {friend_msg}")
-            
-            if friend_msg.lower() == END_CHAT:
-                print("Closing the chat...")
-                socket.close()
-                break      
-    except OSError:
-        raise OSError("Socket error occurred")
-    except Exception:
-        raise Exception("Unknown error occurred")
-
-
-
+        except socket.error as e:
+            raise OSError(f"Error with receiving data:{e}")
+        print(friend_msg)            
+ 
 
 def start(socket):
     try:    
         server_message = socket.recv(HEADER).decode('utf-8')
-
-        if server_message == "Entre name:":
-            name = input('Enter your name:\n')
-            socket.send(name.encode())
-            print('To exit the chat, type the word: EXIT')
-            friend_name = socket.recv(HEADER).decode('utf-8')
-            sender_thread = threading.Thread(target=send_message, args=(socket,))
-            receiver_thread = threading.Thread(target=receive_message, args=(socket, friend_name))
-
-            sender_thread.start()
-            receiver_thread.start()
-
-            sender_thread.join()  
-            receiver_thread.join()
-        
-        else:
-            print('There was an error with the server, try next time.')
-            socket.close()
-            
-    except OSError as ex:
-        raise OSError(ex)
-    except Exception as ex:
-        raise Exception(ex)
+    except socket.error as e:
+            raise OSError(f"Error with receiving data:{e}")
+ 
+    name = input(f'{server_message}:\n')
+    if name == '':
+        socket.close()
+        raise InvalidNameException("You must enter your name, empty value does not accepted.")
+    try:
+        socket.send(name.encode())
+    except socket.error as e:
+            raise OSError(f"Error with sending data:{e}")
     
+    print('To exit the chat, type the word: EXIT')
+    
+    sender_thread = threading.Thread(target=send_message, args=(socket,))
+    receiver_thread = threading.Thread(target=receive_message, args=(socket,))
+
+    sender_thread.start()
+    receiver_thread.start()
+
+    sender_thread.join()  
+    receiver_thread.join()
+
+
 
 
 def main():
@@ -86,7 +76,11 @@ def main():
         logging.debug("client is connecting\n")
         logging.debug("waiting for the server to response\n")
         start(my_socket)
-    
+        
+    except InvalidNameException as ex:
+        print(f"Error: {ex}")
+    except OSError as ex:
+        print(f"Error: {ex}")    
     except argparse.ArgumentError as ex:
         print(f"Error: {ex}")
     except Exception as ex:
